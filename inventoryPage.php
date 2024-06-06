90e33a8b5636b9bde3902aa4e4e080fb4fd14243
caesar0016 <dapitoncaesar26@gmail.com>
2024-06-06 22:32:18 +0800

    added column, still need to sleep na

--- inventoryPage.php
+++ inventoryPage.php
@@ -6,13 +6,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="CSS Files/inventoryPage.css">
+    <link rel="stylesheet" href="cssFiles/inventoryPage.css">
+    <title>Inventory</title>
 </head>
 <body>
-    <h1></h1>
-    <form action="inventoryPage.php" method="post">
-        <button><a href="addItem.php">Add Item</a></button>
-    </form>
+<button type="button" class="btn btn-outline-primary m-2">Add Item</button>
+<table class="table table-hover mt-2">
+<!-- On cells (`td` or `th`) -->
+<tr>
+  <td class="table-primary">#</td>
+  <td class="table-secondary">Category Name</td>
+  <td class="table-success">Price</td>
+  <td class="table-success">Stock</td>
+
+</tr>
+
+<!-- On tables -->
+<table class="table-primary">Hello 1</table>
+<table class="table-primary">Hello 1</table>
+<table class="table-secondary">Secondary</table>
+<table class="table-primary">Hello 1</table>
+
+
+<tr>
+  <td class="table-primary"></td>
+  <td class="table-secondary">Secondary</td>
+  <td class="table-success">Success</td>
+  <td class="table-danger">Danger</td>
+  <td class="table-warning">Warning</td>
+  <td class="table-info">Info</td>
+  <td class="table-light">Light</td>
+  <td class="table-dark">Dark</td>
+</tr>
+
+</table>
 </body>
 </html>
