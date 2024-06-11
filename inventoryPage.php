f83d7a397bee5e0d85d55f579b4bf0bfaf47f836
caesar0016 <dapitoncaesar26@gmail.com>
2024-06-11 16:48:22 +0800

    added rows, which is working, still needs to be connect in database

--- inventoryPage.php
+++ inventoryPage.php
@@ -12,34 +12,38 @@
 <body>
 <button type="button" class="btn btn-outline-primary m-2">Add Item</button>
 <table class="table table-hover mt-2">
-<!-- On cells (`td` or `th`) -->
-<tr>
-  <td class="table-primary">#</td>
-  <td class="table-secondary">Category Name</td>
-  <td class="table-success">Price</td>
-  <td class="table-success">Stock</td>
-
-<!-- This is for tr-->
-</tr>
-
-<!-- On tables -->
-<table class="table-primary">Hello 1</table>
-<table class="table-primary">Hello 2</table>
-<table class="table-secondary">Secondary</table>
-<table class="table-primary">Hello 1</table>
-
-
-<tr>
-  <td class="table-primary"></td>
-  <td class="table-secondary">Secondary</td>
-  <td class="table-success">Success</td>
-  <td class="table-danger">Danger</td>
-  <td class="table-warning">Warning</td>
-  <td class="table-info">Info</td>
-  <td class="table-light">Light</td>
-  <td class="table-dark">Dark</td>
-</tr>
-
-</table>
+        <thead>
+            <tr>
+                <!-- Table headers -->
+                <th class="table-primary">#</th>
+                <th class="table-secondary">Category Name</th>
+                <th class="table-success">Price</th>
+                <th class="table-success">Stock</th>
+            </tr>
+        </thead>
+        <tbody>
+            <!-- Example rows with initial data -->
+            <tr>
+                <td class="table-primary">1</td>
+                <td class="table-secondary">Electronics</td>
+                <td class="table-success">$500</td>
+                <td class="table-success">In Stock</td>
+            </tr>
+              <!-- This is the rows 2-->
+            <tr>
+                <td class="table-primary">2</td>
+                <td class="table-secondary">Furniture</td>
+                <td class="table-success">$1500</td>
+                <td class="table-success">Out of Stock</td>
+            </tr>
+              <!-- This is the rows 3-->
+            <tr>
+                <td class="table-primary">3</td>
+                <td class="table-secondary">Furniture</td>
+                <td class="table-success">$1500</td>
+                <td class="table-success">Out of Stock</td>
+            </tr>
+        </tbody>
+    </table>
 </body>
 </html>
