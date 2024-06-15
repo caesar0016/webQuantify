d3b67d718677d885ffab4b5f69d1ee08fd91ecff
caesar0016 <dapitoncaesar26@gmail.com>
2024-06-15 19:51:29 +0800

    initial design viewItemClick.php

--- viewItemClick.php
+++ viewItemClick.php
@@ -1,4 +1,26 @@
 <?php
     include("header.html");
-    echo "This is the item Click Page";
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container-fluid text-center">
+        <div class="row">
+            <div class="col-5 border">
+                1 out of 2
+            </div>
+            <div class="col-3 border">
+                2 out of 2
+            </div>
+            <div class="col-4 border">
+                wow
+            </div>
+        </div>
+    </div>
+</body>
+</html>
