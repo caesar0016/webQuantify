54b9e67a8b37df2d5cd74e7ad7c464be28005f17
caesar0016 <dapitoncaesar26@gmail.com>
2024-06-16 17:36:35 +0800

    Initial code for calendar

--- viewItemClick.php
+++ viewItemClick.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Item Click</title>
 </head>
 <body>
     <div class="container-fluid text-center mt-4">
@@ -45,7 +45,54 @@
             </div>
             <div class="col-4 border">
                 <h1>Pickup Date</h1><br>
-                
+                <div class="calendar">
+                    <div class="calendar-header">
+                        <span class="month-picker" id="month-picker">
+                            June
+                        </span>
+                        <div class="year-picker">
+                            <span class="year-change" id="prev-year">
+                                <pre><</pre>
+                            </span>
+                            <span id="year">2024</span>
+                            <span class="year-change" id="next-year">
+                                <pre>></pre>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="calendar-body">
+                        <div class="calendar-week-day">
+                            <div>Sun</div>
+                            <div>Mon</div>
+                            <div>Tue</div>
+                            <div>Wed</div>
+                            <div>Thu</div>
+                            <div>Fri</div>
+                            <div>Sat</div>
+                        </div>
+                        <div class="calendar-days">
+                            <div>1</div>
+                            <div>2</div>
+                            <div>3</div>
+                            <div>4</div>
+                            <div>5</div>
+                            <div>6</div>
+                            <div>7</div>
+                            <div>1</div>
+                            <div>2</div>
+                            <div>3</div>
+                            <div>4</div>
+                            <div>5</div>
+                            <div>6</div>
+                            <div>7</div>
+                        </div>
+                    </div>
+                    <div class="calendar-footer">
+                    </div>
+                    <div class="month-list">
+                        
+                    </div>
+                </div><!--calendar Header-->
             </div>
         </div>
     </div>
