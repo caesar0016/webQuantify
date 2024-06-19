bbf621b577f3e02e4a489d08d6255e15b05da0b9
caesar0016 <dapitoncaesar26@gmail.com>
2024-06-20 07:38:44 +0800

    adding home page design and replacing the category icon

--- index.php
+++ index.php
@@ -1,16 +1,32 @@
 <?php
-    include("header.html");
-    include("database.php");
-    echo "This is the home page <>";
-?> 
+include("header.html");
+include("database.php");
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
 </head>
+
 <body>
-    <a href="viewItemClick.php" class="btn btn-outline-primary">View Item</a>
+    <table class="table table-secondary mt-4">
+        <thead>
+            <tr>
+                <th scope="col"><a href="#" class="text-dark text-decoration-none">BUSSINESS AND MANAGEMENT</a></th>
+                <th scope="col"><a href="#" class="text-dark text-decoration-none">ARTS AND SCIENCES</a></th>
+                <th scope="col"><a href="#" class="text-dark text-decoration-none">IT AND ENGINEERING</a></th>
+                <th scope="col"><a href="#" class="text-dark text-decoration-none">TOURISM AND MANAGEMENT</a></th>
+                <th scope="col"><a href="#" class="text-dark text-decoration-none">HOSPITALITY AND MANAGEMENT</a></th>
+                <th scope="col"><a href="#" class="text-dark text-decoration-none">HIGH SCHOOL</a></th>
+            </tr>
+        </thead>
+    </table>
+    <div class="container-fluid text-center">
+        <img src="images/bg_sti.png" alt="Sti cover page" class="img-fluid">
+    </div>
 </body>
+
 </html>
