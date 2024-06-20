4113eb592a426e8d16bbcdedf6fabd6a50997976
Jenjenlili <liyanpark7@gmail.com>
2024-06-20 15:31:07 +0800

    categorytbl

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ include("database.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="indexSample.css">
     <title>Home</title>
 </head>
 
